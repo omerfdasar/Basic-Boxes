0a1a6b2626f730af58ebc7257f2150392e8d90ba
Omer <omerfarukdasar@gmail.com>
2022-06-26 19:09:28 +0200

    serializers start

--- student_api/models.py
+++ student_api/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    number = models.IntegerField(blank=True, null=True)
+    # blank=True for admin dashboard
+    # null=True for db
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
